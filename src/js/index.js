7409b0df50257fcab0eb4719896f47dddc5d20bb
Brad Howes <bradhowes@mac.com>
2018-04-19 22:01:07 +0200

    Buffy!

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,16 @@
 "use strict";
 
-(function (doc) {
+(function () {
 
     var installClickHandlers = function () {
 
-        var navElement = doc.getElementById("nav");
+        var navElement = document.getElementById("nav");
 
         // The click handler just toggles classes to do the showing/hiding
         //
         var clickHandler = function(e) {
-            doc.body.classList.toggle("nav-opened");
-            doc.body.classList.toggle("nav-closed");
+            document.body.classList.toggle("nav-opened");
+            document.body.classList.toggle("nav-closed");
             if (navElement) {
                 navElement.classList.toggle("nav-opened");
                 navElement.classList.toggle("nav-closed");
@@ -19,7 +19,7 @@
 
         // Find elements we wish to tap for click events
         //
-        var elements = Array.prototype.slice.call(doc.querySelectorAll(".menu-button, .nav-cover, .nav-close"));
+        var elements = Array.prototype.slice.call(document.querySelectorAll(".menu-button, .nav-cover, .nav-close"));
         if (elements && elements.length > 0) {
             elements.forEach(function (element) {
                 element.addEventListener('click', clickHandler);
@@ -29,11 +29,11 @@
 
     // When the document is finished loading, install event handlers to show/hide sidebar menu
     //
-    doc.addEventListener("DOMContentLoaded", function (event) {
+    window.document.addEventListener("DOMContentLoaded", function (event) {
         installClickHandlers();
 
         // Set the copyright year at the bottom of our pages
         //
-        doc.getElementById("copyrightYear").innerHTML = (new Date()).getFullYear();
+        window.document.getElementById("copyrightYear").innerHTML = (new Date()).getFullYear();
     });
-})(window.document);
+})();
